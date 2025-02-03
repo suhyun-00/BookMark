4ae4e78db209fc6a732069176200b224d6b6490d
suhyun-00 <unhyns00@gmail.com>
2025-02-03 16:18:29 +0900

    feat: 프로젝트 기초 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import 'dotenv/config';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@types': resolve(__dirname, 'src/types'),
+      '@components': resolve(__dirname, 'src/components'),
+    },
+  },
+});
