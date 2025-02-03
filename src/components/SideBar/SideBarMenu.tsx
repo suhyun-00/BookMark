580fedf19e06ddef96cfc3cf987784c88a760c4d
suhyun-00 <unhyns00@gmail.com>
2025-02-03 21:18:02 +0900

    chore: SideBarMenu hover시 포인터 변경

--- src/components/SideBar/SideBarMenu.tsx
+++ src/components/SideBar/SideBarMenu.tsx
@@ -13,10 +13,10 @@ const SideBarMenu = ({
 }: SideBarMenuProps) => {
   return (
     <button
-      className={`flex w-full items-center justify-start gap-3 rounded-xl px-4 py-2 text-sm font-medium ${active ? "bg-white/60 text-gray-900 inset-shadow-2xs hover:bg-gray-200/60" : "text-gray-600 hover:bg-gray-200/60 hover:text-gray-900 hover:inset-shadow-2xs"}`}
+      className={`flex w-full items-center justify-start gap-3 rounded-xl px-4 py-2 text-sm font-medium hover:cursor-pointer ${active ? "bg-white/60 text-gray-900 inset-shadow-2xs hover:bg-gray-200/60" : "text-gray-600 hover:bg-gray-200/60 hover:text-gray-900 hover:inset-shadow-2xs"}`}
       onClick={() => setCurrentMenu(label)}
     >
-      <Icon className="w-4 h-4" />
+      <Icon className="h-4 w-4" />
       {label}
     </button>
   );
