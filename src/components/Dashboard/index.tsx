f0f98b0fa098cbf5c66e12aa97562ae2c6e4a6d7
suhyun-00 <unhyns00@gmail.com>
2025-02-03 21:17:41 +0900

    feat: Dashboard 레이아웃 구현

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@components/Dashboard/Header";
+import View from "@components/Dashboard/View";
+
+const Dashboard = () => {
+  return (
+    <div className="ml-64 min-h-screen w-screen">
+      <Header />
+      <View />
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,11 @@
 import SideBar from "@components/SideBar";
+import Dashboard from "@components/Dashboard";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen bg-gray-100/60 backdrop-blur-xl">
       <SideBar />
+      <Dashboard />
     </div>
   );
 };
