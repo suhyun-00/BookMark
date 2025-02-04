51149a1e260437c62abe654b350d9678aed2c76b
suhyun-00 <unhyns00@gmail.com>
2025-02-04 21:56:17 +0900

    chore: SearchBar 컴포넌트 디렉토리 변경(Dashboard -> common)

--- src/components/Dashboard/Header.tsx
+++ src/components/Dashboard/Header.tsx
@@ -1,4 +1,4 @@
-import SearchBar from "@components/Dashboard/SearchBar";
+import SearchBar from "@components/common/SearchBar";
 import { Plus } from "lucide-react";
 
 const Header = () => {
@@ -6,8 +6,8 @@ const Header = () => {
     <div className="m-4 flex items-center justify-between whitespace-nowrap">
       <h2 className="p-5 text-2xl font-medium text-gray-900">내 서재</h2>
       <div className="flex items-center gap-3">
-        <SearchBar />
-        <button className="flex h-10 items-center gap-3 rounded-xl bg-gray-900 px-4 text-sm text-white hover:cursor-pointer hover:bg-neutral-700">
+        <SearchBar className="w-64" placeholder="책 검색하기" />
+        <button className="flex h-10 items-center gap-3 rounded-lg bg-gray-900 px-4 text-sm text-white hover:cursor-pointer hover:bg-neutral-700">
           <Plus className="h-4 w-4" />책 등록
         </button>
       </div>
---
+++ src/components/common/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { Search } from "lucide-react";
+
+interface SearchBarProps {
+  name?: string;
+  className?: string;
+  placeholder: string;
+}
+
+const SearchBar = ({ name, className, placeholder }: SearchBarProps) => {
+  return (
+    <div
+      className={`flex items-center justify-center rounded-lg bg-white inset-shadow-sm ${className}`}
+    >
+      <Search className="ml-5 h-3 w-3" />
+      <input
+        type="text"
+        name={name}
+        placeholder={placeholder}
+        className="w-full px-3 py-2.5 text-sm text-gray-500 focus:outline-none"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
