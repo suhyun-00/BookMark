2c936fdbcffdfb473c69155e2a8a6ce48af1dd56
suhyun-00 <unhyns00@gmail.com>
2025-02-12 15:46:09 +0900

    refactor: SearchBar 컴포넌트 삭제 및 재배치

--- src/components/Dashboard/Header.tsx
+++ src/components/Dashboard/Header.tsx
@@ -1,5 +1,4 @@
-import SearchBar from '@components/common/SearchBar';
-import { Plus } from 'lucide-react';
+import { Search, Plus } from 'lucide-react';
 
 interface HeaderProps {
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,7 +9,16 @@ const Header = ({ setIsOpen }: HeaderProps) => {
     <div className="m-4 flex items-center justify-between">
       <h2 className="p-5 text-2xl font-medium">내 서재</h2>
       <div className="flex items-center gap-3">
-        <SearchBar className="w-64" placeholder="책 검색하기" />
+        <form className="flex w-64 items-center justify-center rounded-lg bg-white inset-shadow-sm">
+          <Search className="ml-5 h-3 w-3" />
+          <input
+            type="search"
+            name="keyword"
+            placeholder="책 검색하기"
+            autoComplete="off"
+            className="w-full px-3 py-2.5 text-sm text-gray-500 focus:outline-none"
+          />
+        </form>
         <button
           onClick={() => {
             setIsOpen((isOpen) => !isOpen);
